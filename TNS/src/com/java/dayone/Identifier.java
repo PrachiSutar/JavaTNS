8a6cb46fae033a61142e4589f63d58912a5d22d5
Prachi Sutar <prachisutar72@gmail.com>
2025-02-18 23:06:48 +0530

    Prachi

---
+++ TNS/src/com/java/dayone/Identifier.java
@@ -0,0 +1,32 @@
+package com.java.dayone;
+
+public class Identifier {
+	
+public static void main(String[] args) {
+		
+		
+		//varible declaration - invalid
+		
+	//	int for =13;  keyword should not be as a varible
+		
+	//	int num 1=13; //keyword should have space
+		
+		//int @num3= 23;
+
+		
+		int $num1 = 23;
+		int num =23;
+		
+		int FOR=23;
+		
+		
+		long l=234567890l;
+		System.out.println($num1);
+		
+		String studentName="Shreya";
+		
+		System.out.println("Value is "+studentName);
+				
+	}
+
+}
